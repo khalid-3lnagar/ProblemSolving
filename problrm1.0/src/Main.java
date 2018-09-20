5679c4857c803306bb08f7675c08bb810292d947
Khalid Elnagar <khaledmoon70163@gmail.com>
2018-09-20 14:08:05 +0200

    add solution for problem1

---
+++ problrm1.0/src/Main.java
@@ -0,0 +1,37 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        int num, D, A;
+        char x;
+        Scanner input = new Scanner(System.in);
+        num = input.nextInt();
+        D = 0;
+        A = 0;
+        String s = input.next();
+
+        for (int i = 0; i < num; i++) {
+
+            x = s.charAt(i);
+
+            if (x == 'A')
+                A++;
+            if (x == 'D')
+                D++;
+
+
+        }
+        if (D == A)
+            System.out.println("Friendship");
+        else if (A > D)
+            System.out.println("Anton");
+        else
+            System.out.println("Danik");
+    }
+}
+
+
